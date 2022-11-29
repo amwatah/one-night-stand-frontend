6e17c384d7182ab7297923f0b332e390d5d3e4b4
Amwatah Obuya <jackalltrade2020@gmail.com>
2022-11-29 14:03:26 +0300

    intial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,41 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { Button } from "@mantine/core";
+import React from "react";
+import { Icon } from "@iconify/react";
 
-const Home: NextPage = () => {
+import { Player } from "@lottiefiles/react-lottie-player";
+
+const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="relative flex flex-col items-center justify-center w-screen min-h-[90vh] ">
+      <h3 className=" primary">one night stand</h3>
+      <section className="lottie w-full h-60">
+        <Player
+          src="https://assets4.lottiefiles.com/packages/lf20_iratqmhn.json"
+          background="transparent"
+          speed={1}
+          loop
+          autoplay
+          className=" w-fit h-60"
+        ></Player>
+      </section>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <p>it takes two to tango!</p>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+      <Button
+        component="a"
+        href="https://play.google.com/store/apps/details?id=com.continuummtechnologies.onenightstand&hl=en_US&gl=US"
+        leftIcon={
+          <Icon icon="mdi:google-play" className="text-3xl font-bold" />
+        }
+        className="  bg-[#eb4354] rounded-full hover:animate-rubberBand"
+      >
+        <p className="flex flex-col p-2 ">
+          <span className="">GET IT ON</span>
+          <span className="capitalize ">Google Play</span>
         </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
